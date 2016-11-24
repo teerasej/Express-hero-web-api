d67d4351d96873798562447504052593fe865017
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-11-24 20:45:24 +0700

    Put getContact to active again

--- public/js/controllers/DashboardController.js
+++ public/js/controllers/DashboardController.js
@@ -3,22 +3,16 @@ myApp.controller('DashBoardController', ['$scope','ProfileService', function($sc
     // Call getContacts() and handle success() and error()
     $scope.profiles = [];
 
-    // ProfileService.getContacts()
-    //     .success(function(resObject){
-    //         //alert('ok...');
-    //         console.log(resObject);
-    //         $scope.profiles = resObject;
-    //     })
-    //     .error(function(error){
-    //         alert('อุ๊บส์...');
-    //     })
+    ProfileService.getContacts()
+        .success(function(resObject){
+            //alert('ok...');
+            console.log(resObject);
+            $scope.profiles = resObject;
+        })
+        .error(function(error){
+            alert('อุ๊บส์...');
+        })
 
-    ProfileService.addHero('Peter', 'Jackson')
-            .success(function(result){
-                console.log(result);
-            })
-            .error(function(error){
-                alert('Error');
-            })
+    
 
 }]);
