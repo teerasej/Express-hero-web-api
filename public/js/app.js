e855ae3a036398e72075e2930e3e6445f8c09669
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-11-24 20:45:01 +0700

    Add Routing

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,23 @@
-myApp = angular.module('myApp', []);
+myApp = angular.module('myApp', ['ngRoute']);
 
+
+myApp.config(['$routeProvider', function($routeProvider) {
+    
+    $routeProvider.when('/', {
+            templateUrl: 'views/dashboard.html',
+            controller: 'DashBoardController'
+        })
+
+        // Add parameter to path '/:id'
+        .when('/create-hero', {
+            templateUrl: 'views/create-hero.html',
+            controller: 'HeroController'
+        })
+
+
+        .otherwise({
+            redirectTo: '/'
+        });
+
+
+}]);
